893778e94a5d70744d184ef56ad5747a7f1540e7
alikunda <76674498+alikunda@users.noreply.github.com>
2021-10-20 13:51:40 -0500

    Revert "admin can only delete users"

--- app/src/main/java/com/example/quickwash/home.java
+++ app/src/main/java/com/example/quickwash/home.java
@@ -1,16 +1,33 @@
 package com.example.quickwash;
-
 import android.os.Bundle;
-/**
- User Home page after login in(home.xml)
- */
+import android.view.Menu;
+import android.view.View;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+/**
+ * User Home page after login in(home.xml)
+ */
 public class home extends AppCompatActivity {
+
     private DatabaseManager dbManager;
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home);
+
         dbManager = new DatabaseManager(this);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_user, menu);
+        return true;
+    }
+
+    public void goBack(View v){
+        this.finish();
+
     }
 }
