202f762aab0949cf6c251dae6d6f3fbb88c3f28a
Jose <jpach04@jaguar.tamu.edu>
2021-10-25 21:29:07 -0500

    Start Order Tab WIP

--- app/src/main/java/com/example/quickwash/ui/order/orderFragment.java
+++ app/src/main/java/com/example/quickwash/ui/order/orderFragment.java
@@ -4,13 +4,14 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import com.example.quickwash.databinding.FragmentOrderBinding;
-import android.widget.TextView;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+
+import com.example.quickwash.R;
+import com.example.quickwash.databinding.FragmentOrderBinding;
 
 public class orderFragment extends Fragment {
 
@@ -22,7 +23,11 @@ public class orderFragment extends Fragment {
         myorderViewModel = new ViewModelProvider(this).get(orderViewModel.class);
 
         binding = FragmentOrderBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
+
+
+        String[] clothing_types = getResources().getStringArray(R.array.clothing_types);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter(requireContext(), R.layout.dropdown_item, clothing_types);
+        binding.autoCompleteTextView.setAdapter(arrayAdapter);
 
        // final TextView textView = binding.textOrder;
       //  myorderViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
@@ -31,6 +36,7 @@ public class orderFragment extends Fragment {
             //    textView.setText(s);
            // }
         //});
+        View root = binding.getRoot();
         return root;
     }
 
