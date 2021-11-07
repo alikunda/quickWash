30433b52246688b3584544946653b45dbea7227b
Jose <jpach04@jaguar.tamu.edu>
2021-11-07 15:40:33 -0600

    garment classes updated

--- app/src/main/java/com/example/quickwash/Garment/Garments/Short.java
+++ app/src/main/java/com/example/quickwash/Garment/Garments/Short.java
@@ -3,4 +3,6 @@ package com.example.quickwash.Garment.Garments;
 import com.example.quickwash.Garment.GarmentType;
 
 public class Short extends GarmentType {
+
+    private double price = 4.39;
 }
--- app/src/main/java/com/example/quickwash/Garment/Garments/Skirt.java
+++ app/src/main/java/com/example/quickwash/Garment/Garments/Skirt.java
@@ -3,4 +3,5 @@ package com.example.quickwash.Garment.Garments;
 import com.example.quickwash.Garment.GarmentType;
 
 public class Skirt extends GarmentType {
+    double price = 5.39;
 }
